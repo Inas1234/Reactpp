cb48e559d46161d470ece0c44b24ba3130ef07fb
Inas1234 <inas.malkic@yahoo.com>
2024-04-02 14:19:56 +0200

    Added all Html Elements

--- main.cpp
+++ main.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
-#include "./include/Element.hpp"
+#include "./include/HtmlElements.hpp"
 
-int main(){
+int x = 0;
+
+int main() {
     docBegin;
-
-    Element main("div");
+    Element main = div;
     main.children(
-        Element("button")
-            .text("Click me!")
-            .on("click", [](){
-                std::cout << "Button clicked!" << std::endl;
-            }),
-        Element("button")
-            .text("Click me too!")
-            .on("click", [](){
-                std::cout << "Second button clicked!" << std::endl;
+        h1.text("Hello, World!"),
+        p.text("This is a paragraph."),
+        div.children(
+            h2.text("This is a subheading."),
+            p.text("This is another paragraph.")
+        ),
+        button.text("Click me!").on("click", [](){
+            x++;
+            std::cout << "Count: " << x << std::endl;
+        }),
+        form.children(
+            label.text("Name:"),
+            input.attr("type", "text"),
+            br,
+            label.text("Email:"),
+            input.attr("type", "email"),
+            br,
+            button.text("Submit").attr("type", "submit").on("click", [](){
+                
+                std::cout << "Form submitted!" << std::endl;
             })
+        )
+
     );
 
 
